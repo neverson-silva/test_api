70a90f3d982d18c12a26b6605ed3e80c5ca4ea6b
Neverson Bento da Silva <neversonbs13@gmail.com>
2026-08-25 08:11:08 -0300

    Update greeting message in getHello method

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! We alive';
+    return 'Hello World! We alive in moon';
   }
 }
